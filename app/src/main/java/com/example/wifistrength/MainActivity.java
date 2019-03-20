60f6bbfaa593d12042e583c72821fd8195fa7abf
parshva-b <parshva.barbhaya@somaiya.edu>
2019-03-21 03:08:20 +0530

    Now using External Storage Public Files

--- app/src/main/java/com/example/wifistrength/MainActivity.java
+++ app/src/main/java/com/example/wifistrength/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.wifistrength;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,7 +34,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView display = findViewById(R.id.display);
         display.setMovementMethod(new ScrollingMovementMethod());
-
+//        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS),"logcat.txt");
+//        if(!file.exists()) {
+//            file.mkdir();
+//        }
         File temp = new File(getFilesDir() + "/"+FILE_NAME);
         if (temp.exists()) {
             RandomAccessFile raf = null;
@@ -46,11 +54,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private boolean isExternalStorageWritable() {
+        if(Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Log.v("State","Yes");
+            return true;
+        }
+        else { return false;}
+    }
+
+    public boolean checkPermission(String permission) {
+        int check  = ContextCompat.checkSelfPermission(this, permission);
+        return (check== PackageManager.PERMISSION_GRANTED);
+    }
+
     public void display(View view) {
-        FileInputStream fis = null;
+        File file = new File(Environment.getExternalStorageDirectory(),FILE_NAME);
 
         try {
-            fis = openFileInput(FILE_NAME);
+            FileInputStream fis = new FileInputStream(file);
             InputStreamReader isr = new InputStreamReader(fis);
             BufferedReader br = new BufferedReader(isr);
             StringBuilder s = new StringBuilder();
@@ -72,48 +93,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void compute(View view) {
-//
         WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+
         if(!wifiManager.isWifiEnabled()) {
-//            Log.v("bacchi","wifi bandh hai bacchi");
-//            return;
             wifiManager.setWifiEnabled(true);
         }
+        StringBuilder s = new StringBuilder();
+        s.append("Link name: "+wifiInfo.getSSID()+"\n");
+        s.append("Link Speed: "+wifiInfo.getLinkSpeed()+" Mbps\n");
+        s.append("MAC address: "+wifiInfo.getMacAddress()+"\n");
 
-        for(int i=0;i<60;i++) {
-            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-            int level = wifiInfo.getRssi();
-            //        Log.v("Wifi Strength", "Ans: "+level);
-            TextView ans = (TextView) findViewById(R.id.result);
-            ans.setText(String.valueOf(level)+" dbm");
+        TextView ans = (TextView) findViewById(R.id.result);
+        Log.v("Wifi", s.toString());
+        ans.setText(s.toString());
 
-        FileOutputStream fos = null;
+        if(isExternalStorageWritable() && checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            File file = new File(Environment.getExternalStorageDirectory(),FILE_NAME);
+            Log.v("External file", file.toString());
 
-        try {
-            fos = openFileOutput(FILE_NAME,MODE_APPEND);
-            fos.write(String.valueOf(level).getBytes());
-            fos.write(" dbm\n".getBytes());
-            Toast.makeText(this, "Saved to "+ getFilesDir() + "/" +FILE_NAME,Toast.LENGTH_LONG).show();
-            Thread.sleep(300);
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+
+                int level =0;
+                StringBuilder log = new StringBuilder();
+
+                for(int i=0;i<60;i++) {
+                    wifiInfo = wifiManager.getConnectionInfo();
+                    level = wifiInfo.getRssi();
+                    log.append(String.valueOf(level)+" dbm\n");
+                    Thread.sleep(100);
+                }
+                fos.write(log.toString().getBytes());
+                fos.close();
+                Toast.makeText(this,"File Saved to: "+file.toString(),Toast.LENGTH_LONG).show();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            } finally {
-                if(fos!=null) {
-                    try {
-                        fos.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
             }
 
 
         }
-
+        else {
+            Toast.makeText(this,"Cannot perform write operation, Permission denied",Toast.LENGTH_LONG).show();
+        }
     }
 
 }
